fa73089c42d9cb9288624adcc873495bbd1d85bc
admal <admal@LAPTOP-GGPI73LN>
2021-10-12 23:04:35 +0400

    addi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args){
 
-    public static void main(String[] args) throws IOException {
-        Interface CruiserMoving = new Interface();
     }
 }
